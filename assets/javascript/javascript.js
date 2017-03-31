fac94fd0efcf8c2b14fff2e3258ca98057a13c48
luis paredes <luis paredes>
2017-03-31 14:36:39 -0400

    updated contact page

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -110,4 +110,26 @@ $(document).ready(function() {
 			});
 		}
 	}
+
+	$("#send-email").click(function(event) {
+		event.preventDefault();
+		var name = $("#name-input").val().trim();
+		var email = $("#email-input").val().trim();
+		var message = $("#message-input").val().trim();
+		emailjs.send("gmail", "template_7hezNZIu", {
+			"from_name"    : name,
+			"to_name"      : "Luis Paredes",
+			"reply_to"     : email,
+			"message_html" : message
+		})
+		.then(function(response) {
+			$("#name-input").val("");
+			$("#email-input").val("");
+			$("#message-input").val("");
+
+			$("#message-alert").css({
+				"display": "block",
+			});
+		});
+	});
 });
